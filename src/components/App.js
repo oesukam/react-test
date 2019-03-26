948dca6b887640b7ca094e613e7ab06e71903468
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-03-26 17:45:31 +0200

    First test

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+import uuid from "uuid";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      gifts: []
+    };
+  }
+  addGift = () => {
+    const { gifts } = this.state;
+    gifts.push({ id: uuid.v4() });
+    this.setState({
+      gifts
+    });
+  };
+  renderGifts = () => {
+    return this.state.gifts.map(gift => <div key={gift.id}>gift</div>);
+  };
   render() {
-    return <div />;
+    return (
+      <div>
+        <h2>Gift Giver</h2>
+        <div className="gift-list">{this.renderGifts()}</div>
+        <Button className="btn-add" onClick={this.addGift}>
+          Add gift
+        </Button>
+      </div>
+    );
   }
 }
 
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,9 +2,30 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
 
-const app = shallow(<App />);
 describe("App", () => {
+  const app = shallow(<App />);
   test("should render the APP", () => {
     expect(app).toMatchSnapshot();
+  });
+
+  test("initialise the `state` with empty array", () => {
+    expect(app.state().gifts).toEqual([]);
+  });
+
+  describe("when clicking the `add gift` button", () => {
+    beforeEach(() => {
+      app.find(".btn-add").simulate("click");
+    });
+    afterEach(() => {
+      app.setState({ gifts: [] });
+    });
+    test("add a new gift to `state`", () => {
+      expect(app.state().gifts.length).toBe(1);
+    });
+
+    test("add a new gift to the rendered list", () => {
+      console.log(app.debug());
+      expect(app.find(".gift-list").children().length).toBe(1);
+    });
   });
 });
